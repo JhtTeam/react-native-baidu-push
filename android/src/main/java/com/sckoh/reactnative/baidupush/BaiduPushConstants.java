b1e5263200498ced87662155ac7c919f310d2c1f
soocheng <soocheng.koh@ifastfinancial.com>
2017-08-04 13:56:49 +0800

    init project

---
+++ android/src/main/java/com/sckoh/reactnative/baidupush/BaiduPushConstants.java
@@ -0,0 +1,10 @@
+package com.sckoh.reactnative.baidupush;
+
+public class BaiduPushConstants {
+    public static final String TITLE = "title";
+    public static final String MESSAGE = "message";
+    public static final String DESCRIPTION = "description";
+    public static final String CONTENT_AVAILABLE = "content-available";
+    public static final String CUSTOM_CONTENT = "custom_content";
+    public static final String CONTENT = "content";
+}
